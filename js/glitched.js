125d3c5be678e111b9114b8c86ab42b03a3d76dc
Pablo Fonseca <fonsecasilvapab@gmail.com>
2024-04-29 23:15:01 +0200

    semi-final version I hope

--- js/glitched.js
+++ js/glitched.js
@@ -1,24 +1,3 @@
-
-let hasScrolled = false; // Flag to check if the scroll function has already run
-
-function fixEyeOnFirstScroll() {
-    const eye = document.querySelector('.eye');
-    const rect = eye.getBoundingClientRect(); // Get the eye's position relative to the viewport
-
-    if (!hasScrolled) {
-        // Apply fixed positioning with initial coordinates matched
-        eye.style.position = 'fixed';
-        eye.style.top = `${rect.top}px`;
-        eye.style.left = `${rect.left}px`;
-        eye.style.transform = ''; // Remove transform since it's no longer needed
-
-        hasScrolled = true; // Set flag to prevent re-running
-    }
-}
-
-window.addEventListener('scroll', fixEyeOnFirstScroll, { once: true }); // Use the `{ once: true }` option to only run this listener once
-
-
 var changeState = function(state) {
     document.body.className = "body-state" + state;
     if (state === 2) {
